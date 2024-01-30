94e04f41d3a80416ef53b770403bdc2f8a1cfc7b
Seokhyung Lee <korea970223@naver.com>
2024-01-30 20:49:50 +0900

    :sparkles:Feat : Home 페이지 구현

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/common/header/Header';
+import Main from '../components/main/Main';
+import Carousel from '../components/carousel/Carousel';
+import Footer from '../components/footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Carousel />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
