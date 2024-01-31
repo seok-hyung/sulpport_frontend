f59645a7d644130c06f3a9c51c47c1a2b8d59983
Seokhyung Lee <korea970223@naver.com>
2024-01-31 23:40:38 +0900

    :truck:Add : 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Blessing from './pages/Blessing';
+import Qna1 from './pages/qna/Qna1';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/blessing" element={<Blessing />} />
+      <Route path="/qna1" element={<Qna1 />} />
     </Routes>
   );
 }
