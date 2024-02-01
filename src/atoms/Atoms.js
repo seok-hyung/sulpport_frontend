ce2623801fdc77fa5c9f5a8f069f5b96d95b2db9
Seokhyung Lee <korea970223@naver.com>
2024-02-01 17:53:03 +0900

    :sparkles:Feat : 리코일 설치 및 변수 설정

---
+++ src/atoms/Atoms.js
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+export const blessingNameState = atom({
+  key: 'blessingNameState',
+  default: '',
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const blessingToneState = atom({
+  key: 'blessingToneState',
+  default: '',
+  effects_UNSTABLE: [persistAtom],
+});
