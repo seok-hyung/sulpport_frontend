cbd95bc34ffc2c8828198759f7086454a3afe6f9
Seokhyung Lee <korea970223@naver.com>
2024-02-01 17:56:11 +0900

    :pencil2:Modify : 컴포넌트명 변경

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Blessing from './pages/Blessing';
-import Qna1 from './pages/qna/Qna1';
+import Qna from './pages/qna/Qna';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blessing" element={<Blessing />} />
-      <Route path="/qna1" element={<Qna1 />} />
+      <Route path="/qna" element={<Qna />} />
     </Routes>
   );
 }
