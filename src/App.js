b9d8ea9a6b74f0275b3cbcd365b5a047ffe7950d
Seokhyung Lee <korea970223@naver.com>
2024-02-02 20:24:00 +0900

    :truck:Modify : 라우트 path 변경

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Blessing from './pages/Blessing';
-import Qna from './pages/qna/Qna';
+import BlessingQna from './pages/qna/BlessingQna';
+import BlessingQnaResult from './pages/qna/BlessingQnaResult';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blessing" element={<Blessing />} />
-      <Route path="/qna" element={<Qna />} />
+      <Route path="/blessingQna" element={<BlessingQna />} />
+      <Route path="/blessingQnaResult" element={<BlessingQnaResult />} />
     </Routes>
   );
 }
