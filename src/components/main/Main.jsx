d36f4663ac7f8184b8c309f1be2ae0f29267a576
Seokhyung Lee <korea970223@naver.com>
2024-02-03 13:48:02 +0900

    :lipstick:Modify : 메인 UI 수정

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,78 +1,231 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import Carousel from '../carousel/Carousel';
 
 const Main = () => {
   return (
-    <MainWrapper>
-      <section>
-        <div className="logo">메인 캐릭터 or 로고</div>
-        <p>우리들의 설날을 더욱 알차게!</p>
-        <Text>support 설, 설포트</Text>
-        <BoxContainer>
-          <Box>선물/용돈 추천</Box>
-          <Box>덕담 추천</Box>
-          <Box>설날 상식백과</Box>
-        </BoxContainer>
-      </section>
-    </MainWrapper>
+    <>
+      <MainWrapper>
+        <IntroduceSection>
+          <ul className="menusUl">
+            <li className="blessing">
+              <img src="/assets/logo-pocket.svg" alt="로고 이미지" />
+              <p>덕담 가이드</p>
+            </li>
+            <li className="present">
+              <img src="/assets/present.svg" alt="선물 이미지" />
+              <p>용돈/선물 추천</p>
+            </li>
+            <li className="informtion">
+              <img src="/assets/dictionary-icon.svg" alt="사전 이미지" />
+              <p>설날 상식백과</p>
+            </li>
+          </ul>
+        </IntroduceSection>
+        <Carousel />
+        <TestSection>
+          <h2>
+            <img src="/assets/logo-circle-orange.svg" alt="원모양 로고" />
+            설날 뭐하지? 진단테스트
+          </h2>
+
+          <ul className="testUl">
+            <li className="blessingTestLi">
+              <div className="left">
+                <p className="tags">#설날 연휴 #덕담멘트_고민될_때</p>
+                <img src="/assets/blessing-txt-black.svg" alt="" />
+                <button>덕담 만들기</button>
+              </div>
+              <div className="right">
+                <img src="/assets/logo-pocket.svg" alt="" />
+              </div>
+            </li>
+            <li className="presentTestLi">
+              <div className="left">
+                <p className="tags">#설날 연휴 #뭐줄지_고민될_때</p>
+                <img src="/assets/present-txt-black.svg" alt="" />
+                <button>추천 받기</button>
+              </div>
+              <div className="right">
+                <img src="/assets/present-bird.svg" alt="" />
+              </div>
+            </li>
+          </ul>
+        </TestSection>
+      </MainWrapper>
+    </>
   );
 };
 
 export default Main;
 
 const MainWrapper = styled.main`
-  height: 600px;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   font-size: 24px;
-  section {
+`;
+
+const IntroduceSection = styled.section`
+  margin: 70px auto;
+  .menusUl {
+    width: 1300px;
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-    .logo {
-      padding: 30px 20px;
-      border: 1px solid black;
-      font-size: 26px;
+    text-align: center;
+    gap: 100px;
+    img {
+      width: calc(650px / 3);
     }
-    p {
-      font-size: 32px;
-      font-weight: 600;
-      margin-top: 30px;
-      min-width: 380px;
-      text-align: center;
+    li {
+      border-radius: 20px;
+      padding: 40px;
+      p {
+        margin-top: 20px;
+        font-size: 36px;
+        color: white;
+        font-weight: 700;
+      }
+    }
+    .blessing {
+      background-color: var(--main-color);
+    }
+    .present {
+      background-color: #28afb0;
+    }
+    .informtion {
+      background-color: #37392e;
     }
   }
 `;
-const Text = styled.p`
-  font-size: 32px;
-  font-weight: 600;
-  margin-top: 30px;
 
-  @media (max-width: 800px) {
-    display: none;
-  }
-`;
+const TestSection = styled.section`
+  margin-top: 70px;
+  width: 1300px;
+  margin: 120px auto 0 auto;
 
-const BoxContainer = styled.div`
-  display: none;
-  width: 100%;
-  justify-content: space-between;
-
-  @media (max-width: 800px) {
+  h2 {
     display: flex;
-    margin-top: 30px;
+    align-items: center;
+    justify-content: center;
+    font-size: 36px;
+    img {
+      width: 40px;
+    }
   }
-`;
+  .testUl {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+    .blessingTestLi {
+      display: flex;
+      margin-bottom: 50px;
+      width: 100%;
+      padding: 50px;
+      border-radius: 20px;
+      background: linear-gradient(
+        to bottom,
+        rgba(232, 195, 161, 0.2) 20%,
+        rgba(25, 100, 126, 0.2) 80%
+      );
+      .left {
+        .tags {
+          font-size: 30px;
+          color: #37392e;
+        }
+        img {
+          width: 200px;
+          margin-top: 20px;
+          height: fit-content;
+        }
+        button {
+          background-color: #19647e;
+          color: white;
+          font-size: 24px;
+          display: block;
+          margin-top: 180px;
+          border-radius: 10px;
+          padding: 10px 20px;
+        }
+      }
+      .right {
+        img {
+          width: 450px;
+          margin-left: 250px;
+          /* position: relative;
+          right: 0;
+          top: 0;
 
-const Box = styled.div`
-  width: 30%;
-  border: 1px solid black;
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  line-height: 35px;
+          &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 100px;
+            width: 100%;
+            height: 100%;
+            background-image: url('/assets/logo-pocket.svg');
+            filter: blur(10px);
+            opacity: 0.6;
+            z-index: -1;
+          } */
+        }
+      }
+    }
+    .presentTestLi {
+      display: flex;
+      width: 100%;
+      padding: 50px;
+      border-radius: 20px;
+      background: linear-gradient(
+        to bottom,
+        rgba(232, 195, 161, 0.2) 20%,
+        rgba(25, 100, 126, 0.2) 80%
+      );
+      .left {
+        .tags {
+          font-size: 30px;
+          color: #37392e;
+        }
+        img {
+          width: 300px;
+          margin-top: 20px;
+          height: fit-content;
+        }
+        button {
+          background-color: #19647e;
+          color: white;
+          font-size: 24px;
+          display: block;
+          margin-top: 180px;
+          border-radius: 10px;
+          padding: 10px 20px;
+        }
+      }
+      .right {
+        img {
+          width: 600px;
+          margin-left: 150px;
+
+          /* position: relative;
+
+          &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: -40px;
+            width: 100%;
+            height: 100%;
+            background-image: url('/assets/present-bird.svg');
+            filter: blur(10px);
+            opacity: 0.6;
+            z-index: -1;
+          } */
+        }
+      }
+    }
+  }
 `;
