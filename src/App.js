69c31f53dafe4fd75fc72fcdeb0270fd43203adc
seok-hyung <korea970223@naver.com>
2024-02-03 22:04:14 +0900

    :truck:Add : 라우트 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './pages/Home';
 import Blessing from './pages/Blessing';
 import BlessingQna from './pages/qna/BlessingQna';
 import BlessingQnaResult from './pages/qna/BlessingQnaResult';
+import MoneyPresent from './pages/MoneyPresent';
+import MoneyQna from './pages/qna/MoneyQna';
+import PresentQna from './pages/qna/PresentQna';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Route path="/blessing" element={<Blessing />} />
       <Route path="/blessingQna" element={<BlessingQna />} />
       <Route path="/blessingQnaResult" element={<BlessingQnaResult />} />
+      <Route path="/moneyPresent" element={<MoneyPresent />} />
+      <Route path="/moneyQna" element={<MoneyQna />} />
+      <Route path="/presentQna" element={<PresentQna />} />
     </Routes>
   );
 }
