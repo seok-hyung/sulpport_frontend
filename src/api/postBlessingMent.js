e536c9e76717c9e79bea42b00278e47798ef89e2
seok-hyung <korea970223@naver.com>
2024-02-13 23:21:36 +0900

    :fire:Delete : 불필요한 코드 및 파일 삭제

--- src/api/postBlessingMent.js
+++ src/api/postBlessingMent.js
@@ -6,12 +6,10 @@ export const postBlessingMent = async (formData) => {
       'Content-type': 'application/json',
     },
   });
-  console.log(res);
   if (!res.ok) {
     throw new Error('네트워크에 문제가 있습니다.');
   }
   const data = await res.json();
-  // console.log(data);
 
   return data;
 };
